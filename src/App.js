c6dc90b4c1d3b48fedd9601337b2f5997073d3fb
m-triple-m <triplem656@gmail.com>
2022-09-09 11:01:40 +0530

    setup

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './components/Home';
+import Login from './components/Login';
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import NotFound from './components/NotFound';
+import Header from './components/Header';
+import EventHandling from './components/EventHandling';
+import Todo from './components/Todo';
+import Register from './components/Register';
+import UserManager from './components/UserManager';
+import { Toaster } from 'react-hot-toast';
+
+
+function App() {
+  return (
+    <div>
+      <Toaster position='top-right' />
+      <BrowserRouter>
+        <Header />
+        {/* <Link to="/home">Home</Link>
+        <Link to="/login">Login</Link> */}
+        <Routes>
+
+          <Route element={<Home></Home>} path="/" />
+          <Route element={<Home></Home>} path="home" />
+          <Route element={<Login />} path="login" />
+          <Route element={<EventHandling />} path="event" />
+          <Route element={<Todo />} path="todo" />
+          <Route element={<Register />} path="register" />
+          <Route element={<UserManager />} path="usermanager" />
+          <Route element={<NotFound />} path="*" />
+
+        </Routes>
+      </BrowserRouter>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Todo.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { useState } from 'react';
+import './todo.css';
+
+const Todo = () => {
+
+    const [todoList, setTodoList] = useState(['Eat breakfast', 'Eat lunch', 'Eat Dinner']);
+    const [task, setTask] = useState("");
+
+    const addNewTask = () => {
+        // todoList.push("new task");
+        if(task){
+            setTodoList( [ ...todoList, task ] );
+            setTask("");
+        }
+    }
+
+    const removeTask = (index) => {
+        let temp = todoList;
+        temp.splice(index, 1);
+        // update
+        // temp[i] = newValue;
+        setTodoList([...temp]);
+    }
+
+    const showTodoList = () => {
+        return todoList.map( (task, index) => <div className='d-flex task-body justify-content-between'>
+            <p>{task}</p>
+            <button className='btn btn-danger' onClick={() => { removeTask(index) }}> <i class="fas fa-trash"></i> </button>
+        </div> );
+    }
+
+  return (
+    <div className='container'>
+        <div className="card mt-5">
+            <div className="card-body">
+                <h3>Todo List</h3>
+                <hr />
+
+                <div className="input-group">
+                    <input className='form-control' value={task} onChange={(e) => { setTask(e.target.value) }} />
+                    <button onClick={addNewTask} className='btn btn-primary input-group-append'>Add Task</button>
+                </div>
+                {showTodoList()}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Todo;
